7a5e1627a984a76def09de07a6a8ccb5b1637ad4
simonfea <simon@wedgetech.co.uk>
2018-02-13 20:30:41 +0000

    -Fixed toDMS()
    -Refactored toDMS() to split into different functions
    -More tests

---
+++ src/test/kotlin/uk/co/wedgetech/geodesy/DoubleUtilsKtTest.kt
@@ -0,0 +1,16 @@
+package uk.co.wedgetech.geodesy
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class DoubleUtilsKtTest {
+
+    @Test
+    fun toFixed() {
+    }
+
+    @Test
+    fun toFixed2() {
+    }
+}
